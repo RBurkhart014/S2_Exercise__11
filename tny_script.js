9e26e9900fcd67eb10b2fff21e150645aa658ee7
Ryan Burkhart <RBurkhart014@west-mec.org>
2019-02-05 18:17:52 -0700

    Linked Java Page & Started Basic Repository Set-Up

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,12 @@
+"use strict";
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Ryan Burkhart
+   Date: 2-5-19  
 
 */
-
